ebe34b40d591f6c85e34c78616d33a10deae8b3d
LLLzzzCCC <liuzichao10000@163.com>
2017-11-30 18:39:39 +0800

    1130

---
+++ antun-rest-app/src/main/java/com/zdjy/bigdata/antun/web/ChannelController.java
@@ -0,0 +1,24 @@
+package com.zdjy.bigdata.antun.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zdjy.bigdata.antun.domain.Channel;
+import com.zdjy.bigdata.antun.service.ChannelService;
+import com.zdjy.bigdata.antun.web.response.BaseResponse;
+
+@RestController
+@RequestMapping("/channels")
+public class ChannelController {
+	@Autowired
+	private ChannelService channelService;
+    @RequestMapping(value="",method=RequestMethod.GET)
+    public BaseResponse findAll(){
+    	              List<Channel> channels=channelService.findAll();
+    	return BaseResponse.successResponse("查询成功").data(channels);
+    }
+}
